fa77d4ebae2e7c2687fec64aa4ae870a47cfb307
Pradum Kumar <pradumky803@gmail.com>
2025-12-20 23:03:26 +0530

    Correct route to get IP

--- app/api/ip/route.js
+++ app/api/ip/route.js
@@ -4,11 +4,11 @@ export function GET(req) {
   const forwarded = req.headers.get("x-forwarded-for");
 
   // Locally, we can use a placeholder IP
-    const ip = "192.168.10.1";
+    // const ip = "192.168.10.1";
   // Get the client's IP address In Production
-  //   const ip = forwarded
-//     ? forwarded.split(",")[0].trim()
-//     : req.ip ?? "Unknown";
+    const ip = forwarded
+    ? forwarded.split(",")[0].trim()
+    : req.ip ?? "Unknown";
 
   return NextResponse.json({ ip });
 }
